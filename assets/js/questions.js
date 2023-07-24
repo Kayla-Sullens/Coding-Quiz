66103f92c6f223b192eb268a62e7f51ec8a95bb6
Kayla <sullenspartyof3@gmail.com>
2023-07-24 11:23:02 -0700

    Function to render Q & A to screen added

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,32 +1,80 @@
-var score = 0;
-var container = document.querySelector("#container");
-var quizContent = document.querySelector("quizContent");
-
-
-const questions=[
+// Var with array and object for quiz questions
+var questions=[
     {
-        Q:"How do you add a comment to a CSS or JS file?",
-        A:["*Comment*","//Comment","/*Comment","--Comment--"],
-        C:"//Comment"
+        question:"How do you add a comment to a CSS or JS file?",
+        Choices:["*Comment*","//Comment","/*Comment","--Comment--"],
+        Answer:"//Comment"
     },
     {
-        Q:"What is the first index of an array?",
-        A:["0","1","First","Idk"],
-        C:"0"
+        question:"What is the first index of an array?",
+        Choices:["0","1","First","Idk"],
+        Answer:"0"
     },
     {
-        Q:"Which CSS property gives something rounded corners?",
-        A:["corner-round","rounding","border-radius","border"],
-        C:"border-radius"
+        question:"Which CSS property gives something rounded corners?",
+        Choices:["corner-round","rounding","border-radius","border"],
+        Answer:"border-radius"
     },
     {
-        Q:"What is a short section of code written to complete a task called?",
-        A:["Function","Array","Loop","Variable"],
-        C:"Function"
+        question:"What is a short section of code written to complete a task called?",
+        Choices:["Function","Array","Loop","Variable"],
+        Answer:"Function"
     },
     {
-        Q:"W",
-        A:["a","b","c","d"],
-        C:"b"
+        question:"String values must be enclosed with ___",
+        Choices:["parenthesis","quotes","commas","curly brackets"],
+        Answer:"quotes"
     }
 ];
+// Declared variables
+var score = 0;
+var questionIndex = 0;
+
+// Declared variables and start working code
+var time = document.querySelector("#time");
+var start = document.querySelector("#startQuiz");
+var questions = document.querySelector("#questions");
+var wrapper = document.querySelector("#wrapper");
+
+// Timer variables and create a new element 
+var secondsLeft = 60;
+var holdInterval = 0;
+var penalty = 10;
+var ulCreate = document.createElement("ul");
+
+// Triggers the timer to start
+timer.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setINteval(function () {
+            secondsLeft--;
+            time.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <=0) {
+                clearInterval(holdInterval);
+                allDone();
+                time.textContent = "Out of Time!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
+});
+
+// Renders questions and answer choices to the screen
+function render(questionIndex) {
+    questions.innerHTML = "";
+    ulCreate.innerHTML = "";
+
+    for (var i = 0; i < questions.length; i++) {
+        var userQuestion = questions[questionIndex].question;
+        var userChoices = questions[question.Index].choices;
+        questions.textContent = userQuestion;
+    }
+
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        questions.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
