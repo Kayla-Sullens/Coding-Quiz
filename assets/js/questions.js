1e153529abfd5cda6c4dce0759ecf846ed3eaa0a
Kayla <sullenspartyof3@gmail.com>
2023-08-13 07:03:25 -0700

    Trying to figure out answers issue

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -176,3 +176,32 @@ function allDone() {
             }
         });
     }
+
+var highScore = document.querySelector("#highScores");
+var goBack = document.querySelector("#goBack");
+var clear = document.querySelector("#clear");
+
+
+
+// Get local storage
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+    for (var i = 0; i < allScores.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+    }
+}
+
+// Event listener to go back to index page
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
+
+// Event listener to clear the score
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
